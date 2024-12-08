354569b2a905735aea17f3718049ef587776720a
NebulaMakin <71604973+NebulaMakin@users.noreply.github.com>
2024-12-08 22:47:32 +0530

    Created a prototype of the API, connecting it to the MonoDB using a secret key variable from a configuration file in index.js.

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const app = express();
-const port = process.env.PORT || 5000;
 
+const mongoose = require('mongoose');
+
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+async function main() {
+  await mongoose.connect(process.env.DB_URL);
 // routes
 app.use('/', (req,res) => {
-  res.send("WELCOME TO MY SERVER");
+  res.send("Book Store Server is Running");
 });
+}
+
+main().then(() => console.log("Mongodb connected successfully!")).catch(err => console.log(err));
+
+
 
 app.listen(port, () => {
   console.log('Listening on Port: '+port);
