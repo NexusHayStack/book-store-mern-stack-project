694e2bc051a9304a914d7ae6bde76fe23a72ec8f
NebulaMakin <71604973+NebulaMakin@users.noreply.github.com>
2024-12-09 14:31:53 +0530

    Posted a book into the database successfully using create() function.

--- backend/src/books/book.route.js
+++ backend/src/books/book.route.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const Book = require('./book.model');
 const router = express.Router();
 
 
 // Post a book
 router.post('/create-book', async (req,res) =>{
-  console.log(req.body)
+  try {
+    const newBook = await Book.create(req.body);
+    res.status(200).json({message: "Your Book is posted Successfully",book: newBook});
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  }
 });
+
+
 
 
 module.exports = router;
