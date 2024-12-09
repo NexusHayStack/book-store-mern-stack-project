4f46af0806e05bba2a03e8b6a32bfb16686234f7
NebulaMakin <71604973+NebulaMakin@users.noreply.github.com>
2024-12-09 13:40:41 +0530

    Created a router file to route post req(s) then tested it 'trust me bro'. Then installed and imported cors to build a middleware and connected to frontend url

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 
 const mongoose = require('mongoose');
 
 const port = process.env.PORT || 5000;
 require("dotenv").config();
+
+// Middleware
+app.use(express.json());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}));
+
+const bookRoutes = require('./src/books/book.route');
+
+// Routes
+app.use('/api/books', bookRoutes)
 
 async function main() {
   await mongoose.connect(process.env.DB_URL);
